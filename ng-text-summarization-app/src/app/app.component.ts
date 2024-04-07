dc1969ed30d55af28c90244cc4cc235a94dcab19
Connie Leung <cleung334@gmail.com>
2024-04-07 16:27:47 +0800

    chore: update document page

--- ng-text-summarization-app/src/app/app.component.ts
+++ ng-text-summarization-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SummarizationShellComponent } from './summarization/summarization-shell/summarization-shell.component';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,7 @@ import { SummarizationShellComponent } from './summarization/summarization-shell
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'ng-text-summaration-app';
+  constructor(title: Title) {
+    title.setTitle('Generative Ai - Text Summarization Demo');
+  }
 }
--- ng-text-summarization-app/src/app/app.routes.ts
+++ ng-text-summarization-app/src/app/app.routes.ts
@@ -4,17 +4,20 @@ export const routes: Routes = [
   {
     path: 'summarization-page',
     loadComponent: () => import('./summarization/summarize-page/summarize-page.component')
-      .then((m) => m.SummarizePageComponent)
+      .then((m) => m.SummarizePageComponent),
+    title: 'Text Summarization',
   },
   {
     path: 'summarization-translated-page',
     loadComponent: () => import('./summarization/summarize-translated-page/summarize-translated-page.component')
-      .then((m) => m.SummarizeTranslatedPageComponent)
+      .then((m) => m.SummarizeTranslatedPageComponent),
+    title: 'Translate Text Summarization',
   },
   {
     path: 'summarization-as-list',
     loadComponent: () => import('./summarization/summarize-as-list/summarize-as-list.component')
-      .then((m) => m.SummarizeAsListComponent)
+      .then((m) => m.SummarizeAsListComponent),
+    title: 'Summarization List',
   },
   {
     path: '',
