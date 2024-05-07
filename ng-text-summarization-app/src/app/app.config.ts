abde5bb4922fc50e768691a8682800230cf2259a
Connie Leung <cleung334@gmail.com>
2024-05-08 01:50:58 +0800

    feat: stream the generative ai response

--- ng-text-summarization-app/src/app/app.config.ts
+++ ng-text-summarization-app/src/app/app.config.ts
@@ -1,11 +1,11 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideRouter(routes)
   ]
 };
