e130d40151b0385c4bf6ea3b591fe26f32b85999
Connie Leung <cleung334@gmail.com>
2024-05-28 00:36:57 +0800

    refactor: custom rxjs operator

--- ng-text-summarization-app/src/app/summarization/services/summarization.service.ts
+++ ng-text-summarization-app/src/app/summarization/services/summarization.service.ts
@@ -7,6 +7,25 @@ import { LargeLanguageModelUsed } from '../interfaces/llm-used.interface';
 import { SummarizationResult } from '../interfaces/summarization-result.interface';
 import { Summarization } from '../interfaces/summarization.interface';
 
+function summarizeWebPage(data: Summarization) {
+  return function (source: Observable<SummarizationResult>) {
+    return source.pipe(
+      retry(3),
+      map(({ url='', text }) => ({
+        url,
+        text
+      })),
+      catchError((err) => {
+        console.error(err);
+        return of({
+          url: data.url,
+          result: 'No summarization due to error',
+        });
+      })
+    )
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -27,21 +46,8 @@ export class SummarizationService {
     .pipe(
       filter((data) => !!data.url),
       switchMap((data) =>
-        this.httpService.post<{ url: string; text: string }>(`${config.url}/summarization`, data)
-          .pipe(
-            retry(3),
-            map(({ url='', text }) => ({
-              url,
-              text
-            })),
-            catchError((err) => {
-              console.error(err);
-              return of({
-                url: data.url,
-                result: 'No summarization due to error',
-              });
-            })
-          )
+        this.httpService.post<SummarizationResult>(`${config.url}/summarization`, data)
+          .pipe(summarizeWebPage(data))
       ),
       map((result) => result as SummarizationResult),
     );
@@ -49,31 +55,13 @@ export class SummarizationService {
   bulletPointList$  = toObservable(this.bulletPointsSummization)
     .pipe(
       filter((data) => !!data.url),
-      map((data) => ({ url: data.url })),
       switchMap((data) =>
-        this.httpService.post<{ url: string; text: string }>(`${config.url}/summarization/bullet-points`, data)
-          .pipe(
-            retry(3),
-            map(({ url='', text }) => ({
-              url,
-              text
-            })),
-            catchError((err) => {
-              console.error(err);
-              return of({
-                url: data.url,
-                result: 'No summarization due to error',
-              });
-            })
-          )
+        this.httpService.post<SummarizationResult>(`${config.url}/summarization/bullet-points`, data)
+          .pipe(summarizeWebPage(data))
       ),
       map((result) => result as SummarizationResult),
     );  
-
-  getSupportedLanguages() {
-    return config.languages;
-  }
-
+  
   summarizeText(data: Summarization) {
     this.textSummarization.set(data);
   }
